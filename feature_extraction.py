ca0dbcc1f0e9f957af330b1c748df8ff02a7ae75
MayankKharbanda <kharbanda.mayank@outlook.com>
2020-04-06 18:15:46 +0530

    autocoreelation and norm with lag d

--- feature_extraction.py
+++ feature_extraction.py
@@ -31,3 +31,53 @@ print(skewness)
 
 kurtosis = ((mean_diff_quad/no_of_bits)/((mean_diff_sq/no_of_bits)**2))-3
 print(kurtosis)
+
+
+
+
+
+lag_byte = 2
+
+with open('random_files/dev-random.bin','rb') as fr:
+    
+    byte_start = fr.read(1)
+    
+    fr.seek(lag_byte, 1)
+    byte_lag = fr.read(1)
+    forward = 1
+    
+    partial_sum = 0
+
+    
+    while(byte_start and byte_lag):
+        
+        
+        byte_start_string = format(int.from_bytes(byte_start, 'big'),'08b')
+        byte_lag_string = format(int.from_bytes(byte_lag, 'big'),'08b')
+        
+        for i in range(8):
+            bit_1, bit_2 = int(byte_start_string[i]), int(byte_lag_string[i])
+            partial_sum = partial_sum + ((bit_1-expectation)*(bit_2-expectation))
+        
+        
+        if(forward == 0):
+            byte_start = fr.read(1)
+    
+            fr.seek(lag_byte, 1)
+            byte_lag = fr.read(1)
+            forward = 1
+    
+        else:
+            byte_lag = fr.read(1)
+            
+            fr.seek(-lag_byte, 1)
+            byte_start = fr.read(1)
+            forward = 0
+            
+
+
+auto_correlation = partial_sum/mean_diff_sq
+print(auto_correlation)
+
+norm_auto_correlation = (auto_correlation-expectation)/standard_deviation
+print(norm_auto_correlation)
